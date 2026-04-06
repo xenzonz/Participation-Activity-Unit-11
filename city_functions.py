1e06203e0e1ca9d9fc8e774317128fed7aba2ceb
sam cocquyt <samcocquyt04@gmail.com>
2026-04-05 23:03:46 -0400

    city_functions added

--- city_functions.py
+++ city_functions.py
@@ -1,4 +1,11 @@
 print("helloworld")
 
-def city_country():
-    return 0
+def city_country(city, country, population=None):
+
+    formatted_city = city.strip().title()
+    formatted_country = country.strip().title()
+
+    if population is not None:
+        return f"{formatted_city}, {formatted_country} - population {population}"
+
+    return f"{formatted_city}, {formatted_country}"
