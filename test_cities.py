491e0b33e87b54072ca02136c76b253fc719de40
sam cocquyt <samcocquyt04@gmail.com>
2026-04-05 23:10:14 -0400

    tests added

--- test_cities.py
+++ test_cities.py
@@ -1,4 +1,15 @@
-print("testworld")
+#print("testworld")
 
-def test_testountry():
-    return 0
+from city_functions import city_country
+
+
+def test_city_country() -> None:
+    
+    formatted_name = city_country("santiago", "chile")
+    assert formatted_name == "Santiago, Chile"
+
+
+def test_city_country_population() -> None:
+   
+    formatted_name = city_country("santiago", "chile", population=5000000)
+    assert formatted_name == "Santiago, Chile - population 5000000"
