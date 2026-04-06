19ded668237989712bae0d93951635520b95f0c4
sam cocquyt <samcocquyt04@gmail.com>
2026-04-05 23:05:27 -0400

    printstatements added to city_functions.py

--- city_functions.py
+++ city_functions.py
@@ -9,3 +9,8 @@ def city_country(city, country, population=None):
         return f"{formatted_city}, {formatted_country} - population {population}"
 
     return f"{formatted_city}, {formatted_country}"
+
+def main():
+    print(city_country('santiago', 'chile', population=1919111))
+
+main()
